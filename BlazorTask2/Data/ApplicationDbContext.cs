6c42630f635ebf759590b8a049bd9d8d3dd8e30d
Kacper Wojna <264210@student.pwr.edu.pl>
2024-05-06 18:20:25 +0200

    created movie page

---
+++ BlazorTask2/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BlazorTask2.Components;
+
+namespace BlazorTask2.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<BlazorTask2.Components.Movie> Movie { get; set; } = default!;
+    }
+}
